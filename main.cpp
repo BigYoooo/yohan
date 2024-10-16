3d9e1a82880d351aa850e196b1696282b3a28948
unknown <yohan.gauthier02@gmail.com>
2024-10-16 11:26:32 +0800

    Sauvegarde 2 IRQ

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include "mbed.h"
 #include "PinNames.h"
+#include "gpio_irq_api.h"
 
-DigitalIn  mypin(BUTTON1);
-DigitalOut myled(LED1);
+InterruptIn button(BUTTON1);
+DigitalOut led(LED1);
+
+void turn_light()
+{
+    led = !led;
+}
 
 int main() {
-
+    button.rise(&turn_light);
      while (1) {
-        printf("mypin has value : %d \n\r", mypin.read());
-        myled = mypin;
+        printf("mypin has value : %d \n\r", button.read());
         ThisThread::sleep_for(250);
     }
 }
