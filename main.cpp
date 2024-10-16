b3661af5ee538b4a76ec64d31b40755b0f6c3579
unknown <yohan.gauthier02@gmail.com>
2024-10-16 12:09:19 +0800

    Sauvegarde 4 Freq clignot

--- main.cpp
+++ main.cpp
@@ -7,30 +7,52 @@ using namespace std::chrono;
 InterruptIn button(BUTTON1);
 DigitalOut led(LED1);
 Timer t;
+Ticker flipper;
+int var = 1;
 
+//void turn_on()
+//{
+//    led = 1;
+//    t.start();
+//}
+//
+//void turn_off()
+//{
+//    led = 0;
+//    t.stop();
+//    t.reset();
+//}
 
-void turn_on()
-{
-    led = 1;
-    t.start();
+void flip(){
+    led = !led;
 }
 
-void turn_off()
-{
-    led = 0;
-    t.stop();
-    t.reset();
+void decrement(){
+    var = var - 1;
+    flipper.detach();
+    flipper.attach(&flip, var);
+}
+
+void increment(){
+    var = var + 1;
+    flipper.detach();
+    flipper.attach(&flip, var);
 }
 
 int main() {
-    button.rise(&turn_on);
-    button.fall(&turn_off);
+    button.fall(&decrement);
+    button.rise(&increment);
+    flipper.attach(&flip, var); 
+
+    
+    
+    //    if (duration_cast<milliseconds>(t.elapsed_time()).count() > 0)
+    //    {
+    //        printf("The time taken was %llu milliseconds\n", duration_cast<milliseconds>(t.elapsed_time()).count());
+    //    }
+
      while (1) {
         printf("mypin has value : %d \n\r", button.read());
-        if (duration_cast<milliseconds>(t.elapsed_time()).count() > 0)
-        {
-            printf("The time taken was %llu milliseconds\n", duration_cast<milliseconds>(t.elapsed_time()).count());
-        }
         ThisThread::sleep_for(250);
     }
 }
